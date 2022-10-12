8152e501734c38ec5234f0ed3ec24b63123c12d3
our1314 <our1314@foxmail.com>
2022-10-12 11:24:25 +0800

    变更

--- Test/测试pytorch的卷积内部是否含有非线性激活函数.py
+++ Test/测试pytorch的卷积内部是否含有非线性激活函数.py
@@ -1,7 +1,7 @@
 #coding=gb2312
 
 """ 2022.10.9
-疑问：根据鲁鹏老师的所述，神经网络里的卷积是包含非线性激活的，与普通卷积不一样。
+疑问：根据鲁鹏老师的所述，神经网络里的卷积是包含非线性激活、偏置的，与普通卷积不一样，但非线性激活包含在卷积内部还是外部指定？
 解答：编写代码验证，分别用包含和不包含非线性激活层的网络对同一数据进行运算。
 结论：pytorch的卷积层内部不包含非线性激活，原因：↓
 含有ReLU层的网络输出为0，
@@ -10,6 +10,8 @@
 VGG16的网络结构显示：每个卷积层后都跟了一个relu层，而网页上展示的VGG结构基本都忽略了relu层
 resnet一部分卷积后跟batchnorm+relu，另一部分是与原数据相加后再接relu,因此都有relu，（参考：https://www.modb.pro/db/488020）
 因为卷积也是线性运算，如果没有非线性激活则会退化。
+神经网络的卷积运算为：
+卷积+偏置
 """
 
 import torch.nn
@@ -18,7 +20,7 @@ from torch import nn
 from torch.nn import Module
 import cv2
 import numpy as np
-from torchvision.models import resnet18
+from torchvision.models import resnet18, vgg16
 
 #包含Relu的网络
 class MyNet_Relu(Module):
@@ -55,7 +57,7 @@ net = MyNet()
 for m in net_relu.modules():
     if isinstance(m, nn.Conv2d):
         m.weight.data = torch.ones((1, 1, 3, 3))/9.0
-        m.bias.data = torch.zeros(1)
+        m.bias.data = torch.tensor([1])#torch.zeros(1)
         print(m.weight)
 
 for m in net.modules():
@@ -69,6 +71,7 @@ out_relu = net_relu(d1)
 out = net(d1)
 print(f"out_relu={out_relu},out={out}")
 
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 #numpy计算卷积
 src = d1.numpy()#type:np.ndarray
 src = src.reshape((-1))
