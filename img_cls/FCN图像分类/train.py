fef7a6e3c7aedcb24499fdde6a37234b698ff649
our1314 <our1314@foxmail.com>
2022-12-02 08:06:13 +0800

    xx

--- img_cls/FCN图像分类/train.py
+++ img_cls/FCN图像分类/train.py
@@ -174,7 +174,7 @@ def train(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', default='run/train/exp/weights/best.pth',
+    parser.add_argument('--weights', default='run/train/exp_xray_sot23/weights/best.pth',
                         help='指定权重文件，未指定则使用官方权重！')
     parser.add_argument('--resume', default=False, type=bool,
                         help='True表示从--weights参数指定的epoch开始训练,False从0开始')
