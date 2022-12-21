ab056f97231ce7ffcbd31fe6c35e33628fc0f671
our1314 <our1314@foxmail.com>
2022-12-21 16:33:28 +0800

    xx

---
+++ image_classification/cnn_imgcls/utils/utils.py
@@ -0,0 +1,20 @@
+import os
+
+"""
+获取按时间排序的最后一个文件
+"""
+
+
+def getlastfile(path, ext):
+    if os.path.exists(path) is not True: return None
+    list_file = [path + '/' + f for f in os.listdir(path) if f.endswith(".pth")]
+    if len(list_file) > 0:
+        list_file.sort(key=lambda fn: os.path.getmtime(fn))
+        return list_file[-1]
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    a = getlastfile('D:/桌面/JP')
+    pass
