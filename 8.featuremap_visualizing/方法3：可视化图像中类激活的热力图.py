21b20c3100d59365fff1eeed7e23f4cf974089d2
liulin <liulin@apscd.com>
2023-07-11 19:13:40 +0800

    xx

--- 8.featuremap_visualizing/方法3：可视化图像中类激活的热力图.py
+++ 8.featuremap_visualizing/方法3：可视化图像中类激活的热力图.py
@@ -19,20 +19,58 @@ from torch.nn import Module
 from torch import nn
 import cv2
 import sys
+import torch.nn.functional as F
 sys.path.append("D:/work/program/Python/DeepLearning/test_learn_python")
 from myutils.myutils import *
-
+import torchvision.transforms as transforms
     
 if __name__ == '__main__':
     
-    
-
-
-    def hookfn(module, fea_in, fea_out):
-        pass
 
     net = torchvision.models.resnet18(pretrained=True)
-
-    net.register_full_backward_hook
-
+    preprocess = transforms.Compose([transforms.Resize(256),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor(),
+                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
     
+    feature_map = []
+    def forward_hook(module, fea_in, fea_out):
+            feature_map.append(fea_out)
+    
+    net.layer4.register_forward_hook(hook=forward_hook)
+
+    orign_img = Image.open('D:/work/files/deeplearn_datasets/coco128/images/train2017/000000000625.jpg').convert('RGB')
+    img = preprocess(orign_img)
+    img = torch.unsqueeze(img, 0)
+    with torch.no_grad():
+        out = net(img)
+
+    cls = torch.argmax(out).item()
+    weights = net.fc.weight.data[cls,:]
+    #print(weights)
+    
+
+    w = weights.view(*weights.shape, 1, 1)
+    fea = feature_map[0].squeeze(0)
+    ss = w*fea #type:torch.Tensor
+    ss = ss.sum(0)
+    print(ss)
+    ss = F.relu(ss, inplace=True)#inplace表示覆盖原来的内存
+    print(ss)
+
+    ss = ss/ss.max()
+    print(ss)
+    
+    dd = ss.numpy()
+    print(orign_img.size)
+    aa = cv2.resize(dd, orign_img.size)
+    aa = np.uint8(aa*255)
+    heatmap = cv2.applyColorMap(aa,cv2.COLORMAP_JET)
+    
+    src = pil2mat(orign_img)
+    #dis = src*0.5+heatmap*0.5
+    dis = cv2.addWeighted(src,0.5,heatmap,0.5,gamma=0) #orign_img*0.5+heatmap*0.5
+
+    cv2.imshow('dis', dis)
+    cv2.waitKey()
+    pass
