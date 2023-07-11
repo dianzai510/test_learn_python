89b7c537ffa0c81a9f4fb60f769b044fccba32c8
liulin <liulin@apscd.com>
2023-07-11 15:10:01 +0800

    xx

--- 8.featuremap_visualizing/方法1：可视化中间特征图.py
+++ 8.featuremap_visualizing/方法1：可视化中间特征图.py
@@ -8,21 +8,51 @@
 
 import torch
 import torchvision
-import torchviz
 from torch.nn import Module
+from torch import nn
+import cv2
 
-class net_xray(Module):
-    def __init__(self, pretrained, cls_num=2):
-        super(net_xray, self).__init__()
-        self.resnet = resnet18(pretrained=pretrained)
-        self.resnet.fc = Linear(512, cls_num, bias=True)
-
-    def forward(self, x):
-        x = self.resnet(x)
-        # x = self.softmax(x)
-        return x
+import sys
+sys.path.append("D:/work/program/Python/DeepLearning/test_learn_python")
+from myutils.myutils import *
     
 if __name__ == '__main__':
+    def hookfn(module, fea_in, fea_out):
+        fea_out.squeeze_(0)
+        i=0
+        for ch in fea_out:
+            a = ch.unsqueeze(0)
+            img = tensor2mat(a)
+            #img = cv2.resize(img, None, fx=16, fy=16, interpolation=cv2.INTER_NEAREST)
+            img = cv2.resize(img, None, fx=16, fy=16, interpolation=cv2.INTER_LINEAR)
+            cv2.imshow(f'dis', img)
+            cv2.waitKey()
+            i+=1
+            print(a.shape)
+            pass
+        cv2.waitKey()
+
+
     net = torchvision.models.resnet18(pretrained=True)
+    print(net)
+    print('\n')
+    x = cv2.imread('D:/work/files/deeplearn_datasets/coco128/images/train2017/000000000625.jpg')
+    x = mat2tensor(x)
+    x = x.unsqueeze(0)
+    # x = torch.rand((1,3,256,256))
+    for name, m in net.named_children():
+        print(m)
+        print('#'*100)
+        
+        # if isinstance(m, nn.Conv2d):
+        #     m.register_forward_hook(hook=hookfn)
+        #     break
+        if name =='layer4':
+            m.register_forward_hook(hook=hookfn)
+            break
+        #x = m(x)
     
-    pass
+    x = net(x)
+    
+
+    
