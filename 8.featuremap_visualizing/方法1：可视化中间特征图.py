cd1c2c9404f74d14411837eeba193720f639a800
liulin <liulin@apscd.com>
2023-07-13 09:16:20 +0800

    xx

--- 8.featuremap_visualizing/方法1：可视化中间特征图.py
+++ 8.featuremap_visualizing/方法1：可视化中间特征图.py
@@ -28,10 +28,10 @@ if __name__ == '__main__':
     def hookfn(module, fea_in, fea_out):
         fea.append(fea_out)
 
-    net = torchvision.models.resnet18(pretrained=True)
-    net.conv1.register_forward_hook(hook=hookfn)
+    net = torchvision.models.resnet50(pretrained=True)
+    net.layer2.register_forward_hook(hook=hookfn)
 
-    src = Image.open('image/2.jpg').convert('RGB')
+    src = Image.open('image/dog2.jpeg').convert('RGB')
     x = preprocess(src)
     x = x.unsqueeze(0)    
     x = net(x)
@@ -40,14 +40,22 @@ if __name__ == '__main__':
     for i,ch in enumerate(f):
         ch = ch.unsqueeze(0)
         img = tensor2mat(ch)
-        img = cv2.resize(img, None, fx=1, fy=1, interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(img, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
         img = cv2.applyColorMap(img,cv2.COLORMAP_JET)
         cv2.imshow(f'dis{i}', img)
-        rowcnt = 1920//120
+
+        winH,winW = 1080,1920
+        imgH,imgW = img.shape[0],img.shape[1]
+        rowcnt = winW//imgW
         x = i%rowcnt
         y = i//rowcnt
-        cv2.moveWindow(f'dis{i}',x*120,y*120)
+        
+        if y*imgH > winH-imgH:
+            break
+
+        cv2.moveWindow(f'dis{i}',x*imgW,y*imgH)
         cv2.waitKey(1)
+
     cv2.waitKey()
     
 
