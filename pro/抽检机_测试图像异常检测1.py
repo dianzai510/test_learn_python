615336f658c8fdb080d7f1ae4e048434a539c459
our1314 <our1314@foxmail.com>
2023-07-19 15:28:39 +0800

    xx

--- pro/抽检机_测试图像异常检测1.py
+++ pro/抽检机_测试图像异常检测1.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 
-path = 'D:/desktop/tmp2.png'#input('输入图像路径：')
+path = 'D:/desktop/tmp3.png'#input('输入图像路径：')
 src = cv2.imdecode(np.fromfile(path, dtype=np.uint8), cv2.IMREAD_COLOR)#type:np.ndarray
 
 dir_image = "D:\work\proj\抽检机\program\ChouJianJi\data\ic"
@@ -16,16 +16,26 @@ shuffle(images_path)#随机排序
 
 imgs = [cv2.imdecode(np.fromfile(f, dtype=np.uint8), cv2.IMREAD_COLOR) for f in images_path]
 imgs = np.array(imgs)
+feas = imgs.copy()
+
+feas = np.array([cv2.resize(im, None, fx=1, fy=1) for im in imgs])
+gray = np.array([cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) for im in feas])
+sobelx = np.array([cv2.Sobel(im, cv2.CV_32F, 1, 0) for im in gray])
+sobely = np.array([cv2.Sobel(im, cv2.CV_32F, 0, 1) for im in gray])
+sobelx = sobelx[:,:,:,None]
+sobely = sobely[:,:,:,None]
+feas = np.concatenate((feas, sobelx, sobely), axis=3)
+
 
 clf = LocalOutlierFactor(n_neighbors=80, contamination=0.01)#异常检测器
 
 
 #region 遍历每个像素点，统计异常点所在的索引和坐标
-result = np.zeros(imgs.shape[:3])
-_,rows,cols,_=imgs.shape
+result = np.zeros(feas.shape[:3])
+_,rows,cols,_=feas.shape
 for r in np.arange(rows):
     for c in np.arange(cols):
-        a = imgs[:,r,c,:]
+        a = feas[:,r,c,:]
         pred = clf.fit_predict(a)
         ng_index = [i for i,p in enumerate(pred) if p<0]
         for i in ng_index:
