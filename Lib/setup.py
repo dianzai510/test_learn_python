3f5f207c590b3226bec1c3f5c745b9593cf409ab
our1314 <our1314@foxmail.com>
2023-07-25 11:12:08 +0800

    xx

--- Lib/setup.py
+++ Lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='our1314',
-    version='0.1.2',
+    version='0.1.3',
     keywords='our1314',
     description='a library for work',
     license='MIT License',
