3cf691c90a4ac4f9633303a28058cf0b1b2dbbb7
liulin <liulin@apscd.com>
2023-07-30 19:05:50 +0800

    xx

--- 3.image_segmentation/test_unet/data.py
+++ 3.image_segmentation/test_unet/data.py
@@ -92,4 +92,4 @@ if __name__ == '__main__':
 
     data_loader = DataLoader(data, batch_size=1, shuffle=True)
     for image, label in data_loader:
-        torchvision.transforms.ToPILImage()(image[0]*label[0]).show()
+        F.to_pil_image(image[0]*label[0]).show()
