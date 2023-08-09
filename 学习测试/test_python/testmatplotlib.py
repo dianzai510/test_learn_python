837cfd34317336b193b9728caf19c07dc46ef4c7
liulin <liulin@apscd.com>
2023-08-09 08:11:10 +0800

    xx

--- 学习测试/test_python/testmatplotlib.py
+++ 学习测试/test_python/testmatplotlib.py
@@ -2,29 +2,27 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-# X=np.linspace(-np.pi,np.pi,256,endpoint=True)#-π to+π的256个值
-# C,S=np.cos(X),np.sin(X)
-# plt.plot(X,C)
-# plt.plot(X,S)
-# #在ipython的交互环境中需要这句话才能显示出来
-# plt.show()
- 
+#1、绘制简单二维函数
+x = np.linspace(-np.pi, np.pi, 500)
+y1,y2 = np.cos(x),np.sin(x)
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.show()
  
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# # Make data
-# u = np.linspace(0, 2 * np.pi, 100)
-# v = np.linspace(0, np.pi, 100)
-# x = 10 * np.outer(np.cos(u), np.sin(v))
-# y = 10 * np.outer(np.sin(u), np.sin(v))
-# z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+# Make data
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+x = 10 * np.outer(np.cos(u), np.sin(v))
+y = 10 * np.outer(np.sin(u), np.sin(v))
+z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
  
-# # Plot the surface
-# ax.plot_surface(x, y, z, color='b')
- 
-# plt.show()
+# Plot the surface
+ax.plot_surface(x, y, z, color='b')
+plt.show()
 
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
@@ -35,7 +33,7 @@ import matplotlib.pyplot as plt
 mpl.rcParams['legend.fontsize'] = 10
  
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca()
 theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
 z = np.linspace(-2, 2, 100)
 r = z**2 + 1
--- 课程：学习、仿真、测试/线性代数：坐标变换.py
+++ 课程：学习、仿真、测试/线性代数：坐标变换.py
@@ -1,6 +1,13 @@
 import math
 import numpy as np
 
+vector_a = np.array([1, 2, 3, 4, 5])
+vector_b = np.array([1, 2, 3, 4, 5])
+result = vector_a * vector_b[:, None]
+print('向量a与向量b的外积为：')
+print(result)
+
+
 pt = np.array([[5],[4],[1]])  # 二维点的齐次坐标
 H = np.array(
     [
