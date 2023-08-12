3430348f649ddd09dbd2150111f8eaf347a83a75
our1314 <our1314@foxmail.com>
2023-08-12 08:30:13 +0800

    xx

---
+++ 10.proj/极大似然估计.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean = 10
+sigma = 5
+x = np.random.randn(100)
+x = x*sigma+mean
+
+plt.hist(x,bins=50)#直方图，绘制每个数值出现的次数，可以直接表达概率分布情况
+plt.show()
+
