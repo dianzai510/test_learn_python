77601556ecb4c4f31181494ffa3053b848ff2b24
our1314 <our1314@foxmail.com>
2023-08-14 10:18:43 +0800

    xx

--- 课程：学习、仿真、测试/概率统计：极大似然估计.py
+++ 课程：学习、仿真、测试/概率统计：极大似然估计.py
@@ -5,11 +5,15 @@ mean = 10
 sigma = 5
 x = np.random.randn(100)
 x = x*sigma+mean
+x = x.tolist()#type:list
+x[0] = -20.0
 
 plt.hist(x,bins=50)#直方图，绘制每个数值出现的次数，可以直接表达概率分布情况
 plt.show()
 
 miu = np.mean(x)
 sigma = np.std(x)
-print(f'miu={miu},sigma={sigma}')
+ee = x-miu
+ee = ee[np.abs(ee)>3*sigma]
+print(f'miu={miu},sigma={sigma},{ee}')
 pass
