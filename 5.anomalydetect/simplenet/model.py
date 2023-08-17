01ce18075744834f81fda665b79caff361d9c604
our1314 <our1314@foxmail.com>
2023-08-17 13:50:07 +0800

    xx

--- 5.anomalydetect/simplenet/model.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchvision.models.resnet import resnet50, ResNet50_Weights,wide_resnet50_2,Wide_ResNet50_2_Weights
-class SimpleNet(torch.nn.Module):
-    def __init__(self, device):
-        self.backbone = wide_resnet50_2(weights=Wide_ResNet50_2_Weights.DEFAULT)
-        self.layers_to_extract_from = None
-        
-    def forward(self, x):
-        pass
