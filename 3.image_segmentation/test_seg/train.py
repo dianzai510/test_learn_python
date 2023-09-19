c40e8609263e15cec306c056a9174c2f59ee6839
our1314 <our1314@foxmail.com>
2023-09-19 14:35:59 +0800

    xx

--- 3.image_segmentation/test_seg/train.py
+++ 3.image_segmentation/test_seg/train.py
@@ -64,7 +64,7 @@ def train(opt):
             
             loss_train += loss.item()
 
-        scheduler.step()
+        #scheduler.step()
 
         # 验证
         net.eval()
@@ -96,13 +96,13 @@ def train(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pretrain', default='', help='指定权重文件，未指定则使用官方权重！')  # 修改
+    parser.add_argument('--pretrain', default='best_kongdong_304.pth', help='指定权重文件，未指定则使用官方权重！')  # 修改
     parser.add_argument('--out_path', default='./run/train', type=str)  # 修改
     parser.add_argument('--weights', default='best_kongdong_304.pth', help='指定权重文件，未指定则使用官方权重！')
 
     parser.add_argument('--resume', default=False, type=bool, help='True表示从--weights参数指定的epoch开始训练,False从0开始')
-    parser.add_argument('--data_path_train', default='D:/work/files/deeplearn_datasets/xray空洞检测/空洞检测生成数据集/train')
-    parser.add_argument('--data_path_val', default='D:/work/files/deeplearn_datasets/xray空洞检测/空洞检测生成数据集/val')
+    parser.add_argument('--data_path_train', default='D:/work/files/deeplearn_datasets/xray空洞检测/空洞检测生成数据集/备份1/train')
+    parser.add_argument('--data_path_val', default='D:/work/files/deeplearn_datasets/xray空洞检测/空洞检测生成数据集/备份1/val')
     parser.add_argument('--epoch', default=1000, type=int)
     parser.add_argument('--lr', default=0.001, type=float)
     parser.add_argument('--batch_size', default=8, type=int)
